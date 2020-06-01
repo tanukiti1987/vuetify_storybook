3c5f51f1529418e3ee060dbc9efa23eed04177be
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-06-01 22:38:11 +0900

    Add .story/main.js

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport',
+    '@storybook/addon-notes',
+    '@storybook/addon-knobs',
+    'storybook-addon-vue-info/lib/register',
+  ],
+}
